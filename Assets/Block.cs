f1f4ab5e61e25f6971d46947f239627124f5206b
Eihiko <oeihiko@gmail.com>
2016-02-26 21:09:37 +0000

    Upload Pitpex project

---
+++ Assets/Block.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour {
+
+    public Text score;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    void FixedUpdate()
+    {
+        Vector2 p = transform.position;
+        p.x += Input.GetAxis("Mouse X");
+        p.y += Input.GetAxis("Mouse Y");
+        Pitpex.AddScore((int)(p - (Vector2)transform.position).magnitude);
+        score.text = Pitpex.GetScore().ToString();
+        transform.position = p;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void GameOver()
+    {
+        Pitpex.GameOver();
+    }
+}
